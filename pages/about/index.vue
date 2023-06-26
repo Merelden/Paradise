571a24bff996c761d524dcbc5f3ca22de87997e3
Sergey <merelden.77@gmail.com>
2023-06-26 20:20:56 +0500

    create project

---
+++ pages/about/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="">
+      <h1>
+        Hey, we`re Paradise. See your thoughts, stories and ideas.
+      </h1>
+      <p>
+        Paradise is minimal multipurpose blog. A beautiful way to share stories with growing audience.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+
+</style>
